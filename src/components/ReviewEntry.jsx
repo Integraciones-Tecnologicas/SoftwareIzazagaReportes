b11b61be017a3e7a0a05489e5b9918f2989184b5
Gerardo Antonio Ruelas Hernández <gahdz14@gmail.com>
2025-03-03 15:31:02 -0600

    Ahora se pueden seleccionar entradas pendientes en Entrada y Citas

--- src/components/ReviewEntry.jsx
+++ src/components/ReviewEntry.jsx
@@ -158,7 +158,7 @@ const ReviewEntry = () => {
                                             <td className="p-2 flex space-x-2">
                                                 <label className="relative inline-flex items-center cursor-pointer">
                                                     <input type="checkbox" className="sr-only peer" />
-                                                    <div className="peer outline-none duration-100 after:duration-500 w-28 h-14 bg-blue-300 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-500 after:content-['No'] after:absolute after:outline-none after:h-12 after:w-12 after:bg-white after:top-1 after:left-1 after:flex after:justify-center after:items-center after:text-sky-800 after:font-bold peer-checked:after:translate-x-14 peer-checked:after:content-['Sí'] peer-checked:after:border-white"></div>
+                                                    <div className="peer outline-none duration-100 after:duration-300 w-28 h-14 bg-blue-300 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-500 after:content-['No'] after:absolute after:outline-none after:h-12 after:w-12 after:bg-white after:top-1 after:left-1 after:flex after:justify-center after:items-center after:text-sky-800 after:font-bold peer-checked:after:translate-x-14 peer-checked:after:content-['Sí'] peer-checked:after:border-white"></div>
                                                 </label>
                                             </td>
                                         </tr>
